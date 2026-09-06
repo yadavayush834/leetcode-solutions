b9f0122e55f06708fc5a2987bfcec0aa84ff5b3d
Ayush Yadav <yadavayush834@users.noreply.github.com>
2026-09-06 13:38:19 +0530

    solve: 2703. Return Length of Arguments Passed (Difficulty: Easy | Language: javascript)

---
+++ 2703-return-length-of-arguments-passed/solution.js
@@ -0,0 +1,10 @@
+// 2703. Return Length of Arguments Passed
+// https://leetcode.com/problems/return-length-of-arguments-passed/
+// Difficulty: Easy | Language: javascript
+// Runtime: 39 ms
+// Memory: 53.8 MB
+// Solved: 2026-04-18
+
+var argumentsLength = function(...a) {
+    return a.length;
+};
