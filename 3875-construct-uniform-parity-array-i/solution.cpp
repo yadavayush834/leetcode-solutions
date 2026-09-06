decaf2709064f07a33428525ac9ff1adfb5ed95b
Ayush Yadav <yadavayush834@users.noreply.github.com>
2026-09-06 13:38:21 +0530

    solve: 3875. Construct Uniform Parity Array I (Difficulty: Easy | Language: cpp)

---
+++ 3875-construct-uniform-parity-array-i/solution.cpp
@@ -0,0 +1,13 @@
+// 3875. Construct Uniform Parity Array I
+// https://leetcode.com/problems/construct-uniform-parity-array-i/
+// Difficulty: Easy | Language: cpp
+// Runtime: 0 ms
+// Memory: 30.3 MB
+// Solved: 2026-09-02
+
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        return true;
+    }
+};
