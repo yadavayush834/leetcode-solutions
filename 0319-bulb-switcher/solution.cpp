e213260051ed300d6db1ff09181eaae578ca50ae
Ayush Yadav <yadavayush834@users.noreply.github.com>
2026-09-06 13:38:08 +0530

    solve: 319. Bulb Switcher (Difficulty: Medium | Language: cpp)

---
+++ 0319-bulb-switcher/solution.cpp
@@ -0,0 +1,14 @@
+// 319. Bulb Switcher
+// https://leetcode.com/problems/bulb-switcher/
+// Difficulty: Medium | Language: cpp
+// Runtime: 0 ms
+// Memory: 7.7 MB
+// Solved: 2025-09-22
+
+class Solution {
+public:
+    int bulbSwitch(int n) {
+         return sqrt(n);
+        
+    }
+};
